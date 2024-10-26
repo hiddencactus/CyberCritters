064363aa097fb727f54acd090b15d264629bfe40
Bryan <bryancui.111@gmail.com>
2024-10-26 14:38:55 -0400

    erm

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
+  var a = 3 + 4
+
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
