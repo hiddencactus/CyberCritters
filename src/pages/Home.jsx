5d61584fb0472618685714aae8f27447c38caee5
catherineyu0611 <yu359@mcmaster.ca>
2024-10-26 19:01:49 -0400

    styled home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,12 @@ function Home() {
   const navigate = useNavigate()
 
   return (
-    <div>
-        <div className="border border-black" onClick={() => navigate("/game")}>
-            Cyber Critters
+    <div className="h-screen w-screen flex bg-purple-900 justify-center pt-[20%] relative">
+      <div>
+          <h1 className="flex justify-center text-8xl text-lime-500 font-bold tracking-widest p-6">CYBERCRITTERS</h1>
+          <div className="flex justify-center">
+            <button className="bg-lime-500 border border-white text-purple-900 text-3xl font-bold tracking-widest p-4 m-4" onClick={() => navigate("/game")}> START GAME </button>
+          </div>
         </div>
     </div>
   )
